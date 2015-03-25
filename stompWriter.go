711b1f6fd5d8af87df37abc8b4f324b904203dbe
Daniel Hess <dan9186@gmail.com>
2015-03-25 01:28:00 -0700

    preventing panic by handling errors

--- stompWriter.go
+++ stompWriter.go
@@ -1,7 +1,6 @@
 package stompWriter
 
 import (
-	"fmt"
 	"net"
 	"sync"
 	"time"
@@ -50,7 +49,10 @@ func New(hostname, port, username, password, queueName string) (*StompWriter, er
 		"heart-beat", "5000,5000",
 	}
 
-	newStompWriter.Connect()
+	err := newStompWriter.Connect()
+	if err != nil {
+		return nil, err
+	}
 
 	// vvv provides hard reconnect on timeout for DNS change robustness
 	go func(s *StompWriter) {
@@ -65,20 +67,24 @@ func New(hostname, port, username, password, queueName string) (*StompWriter, er
 	return &newStompWriter, nil
 }
 
-func (s *StompWriter) Connect() {
+func (s *StompWriter) Connect() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.Disconnect()
 	// connect IO
 	netCon, err := net.Dial("tcp", net.JoinHostPort(s.hostname, s.port))
 	if err != nil {
-		fmt.Printf("Couldn't connect to stomp host: %v\n", err.Error())
-		return
+		return err
 	}
 	s.netCon = netCon
 
 	// vvv done with helper to facilitate mocking in tests
-	s.Connection = getStompConnection(s.netCon, s.connectionHeaders)
+	s.Connection, err = getStompConnection(s.netCon, s.connectionHeaders)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *StompWriter) Disconnect() {
@@ -102,18 +108,17 @@ func (s *StompWriter) Write(payload []byte) (int, error) {
 	err := s.Connection.Send(h, string(payload))
 	s.mu.Unlock()
 	if err != nil {
-		fmt.Printf("error sending stomp message: %v\n", err.Error())
+		return 0, err
 	}
 
 	return len(payload), nil
 }
 
 // vvv this is a helper to facilitate testing
-var getStompConnection = func(netCon net.Conn, connectionHeaders stompngo.Headers) stompConnectioner {
+var getStompConnection = func(netCon net.Conn, connectionHeaders stompngo.Headers) (stompConnectioner, error) {
 	connection, err := stompngo.Connect(netCon, connectionHeaders)
 	if err != nil {
-		fmt.Printf("Couldn't Connect to stomp host: %v\n", err.Error())
-		return nil
+		return nil, err
 	}
-	return connection
+	return connection, nil
 }
